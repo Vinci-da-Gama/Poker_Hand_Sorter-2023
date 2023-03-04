0808518028d3a17b0f13f46155a363d0aac6d6d5
Boa Mia <boamia@Boas-MacBook-Pro.local>
2023-03-04 15:23:25 +1100

    phs: remove empty lines

--- README.md
+++ README.md
@@ -9,11 +9,11 @@ Requires [Python](https://www.python.org/downloads/)
 ### Run
 
 ```sh
-git clone https://github.com/Vinci-da-Gama/Poker_Hand_Sorter-2023.git
-cd Poker_Hand_Sorter-2023
-
+- git clone https://github.com/Vinci-da-Gama/Poker_Hand_Sorter-2023.git
+- cd Poker_Hand_Sorter-2023
+-
 ```
-Development steps:
+### Development steps:
 
 1. Read the File, then count how many lines are in that file, if the line is empty, then clear the empty line, and then keep reading the rest to the end of the file. When the read is completed, then print out how many lines are read.
 
--- readFile.py
+++ readFile.py
@@ -5,8 +5,15 @@ test_file = input("Enter Pocker Hand Sorter path to test file: ")
 
 # Open file, read its contents
 with open(test_file, 'r') as f:
-    file_contents = f.read()
+  lines = f.readlines()
+
+# Remove empty lines
+lines = [line.strip() for line in lines if line.strip()]
 
 # Print number of lines in the test file
-num_lines = len(file_contents.split('\n'))
-print(f"File {test_file} has been read. {num_lines} lines have been read.")
+num_lines = len(lines)
+total_lines_printout = '''
+input number is: {}
+total lines number is: {}
+'''.format(str(test_file), str(num_lines))
+print(total_lines_printout)
